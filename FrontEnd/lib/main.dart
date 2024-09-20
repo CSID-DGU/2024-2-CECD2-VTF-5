ed7e8dd82962372a500a6f27f6e09a983e41f6a8
Jeong_Hwanuk <udown0109@naver.com>
2024-09-21 02:24:54 +0900

    gpt test

--- FrontEnd/lib/main.dart
+++ FrontEnd/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:vtfecho/model/gptchat.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -34,10 +38,102 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController messageTextController = TextEditingController();
 
+  final List<Messages> _historyList = List.empty(growable: true);
+
+  String apiKey = '';
+  String streamText = '';
+
   static const String _kStrings = "chat bot";
-  String get _currentString => _kStrings;
 
   ScrollController scrollController = ScrollController();
+
+  void _scrollDown() {
+    scrollController.animateTo(scrollController.position.maxScrollExtent,
+        duration: const Duration(microseconds: 300), curve: Curves.fastOutSlowIn);
+  }
+
+  Future requestChat(String text) async {
+    ChatModel openAImodel = ChatModel(
+      messages: [
+        Messages(role: "system", content: "HI GPT"),
+        ..._historyList,
+      ],
+      model: "gpt-4o",
+      stream: false,
+    );
+    final url = Uri.https("api.openai.com","v1/chat/completions");
+    final reponse = await http.post(
+        url,
+        headers: <String, String>{
+      "Authorization":"Bearer $apiKey",
+      "Content-Type":"application/json",
+    },
+    body: jsonEncode(openAImodel.toJson())
+    );
+    print(reponse.body);
+    if(reponse.statusCode==200){
+      final jsonData = jsonDecode(utf8.decode(reponse.bodyBytes)) as Map;
+      String role = jsonData["choices"][0]["message"]["role"];
+      String content = jsonData["choices"][0]["message"]["content"];
+      _historyList.last = _historyList.last.copyWith(role: role, content: content);
+      setState(() {
+        _scrollDown();
+      });
+    }
+  }
+
+  Stream requestChatStream(String text)async*{
+    ChatModel openAImodel = ChatModel(
+      messages: [
+        Messages(role: "system", content: "HI GPT"),
+        ..._historyList,
+      ],
+      model: "gpt-4o",
+      stream: true,
+    );
+
+    final url = Uri.https("api.openai.com","/v1/chat/completions");
+    final request = http.Request("POST",url)
+    ..headers.addAll({
+      "Content-Type":"application/json; charset=UTF-8",
+      "Authorization":"Bearer $apiKey",
+      "Connection": "keep-alive",
+      "Accept-Encoding":"gzip, deflate, br",
+    });
+    request.body=jsonEncode(openAImodel.toJson());
+
+    final response = await http.Client().send(request);
+    final byteStream = response.stream.asyncExpand((event)=>Rx.timer(event,
+    const Duration(microseconds: 50)));
+
+    var restext ="";
+
+    await for (final byte in byteStream){
+      var decode = (utf8.decode(byte, allowMalformed: false));
+      final strings = decode.split("data: ");
+      for(final string in strings){
+        final trimmedString = string.trim();
+        if(trimmedString.isNotEmpty&&!trimmedString.endsWith("[DONE]")){
+          final map=jsonDecode(trimmedString) as Map;
+          final choices = map["choices"] as List;
+          final delta = choices[0]["delta"] as Map;
+          if(delta["content"]!=null){
+            final content = delta["content"]as String;
+            restext += content;
+            setState(() {
+              streamText=restext;
+            });
+            yield content;
+          }
+        }
+      }
+    }
+    if(restext.isNotEmpty){
+      setState(() {
+
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -46,6 +142,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
     super.dispose();
+  }
+
+  Future clearChat() async{
+    showDialog(context: context, builder: (context)=>AlertDialog(
+      title: const Text("new chat"),
+      content: const Text('realy?'),
+      actions: [
+        TextButton(onPressed: (){
+          Navigator.of(context).pop();
+          setState(() {
+            messageTextController.clear();
+            _historyList.clear();
+          });
+        }, child: const Text("yes"))
+      ],
+    ));
   }
 
   @override
@@ -65,50 +177,111 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Card(
                   child: PopupMenuButton(itemBuilder: (context) {
                     return [
-                      PopupMenuItem(
+                      const PopupMenuItem(
                           child: ListTile(
-                        title: const Text("히스토리"),
+                        title: Text("히스토리"),
                       )),
                       PopupMenuItem(
-                          child: ListTile(
-                        title: const Text("새 채팅"),
+                        onTap: (){
+                          clearChat();
+                        },
+                          child: const ListTile(
+                        title: Text("새 채팅"),
                       ))
                     ];
                   }),
                 ),
               ),
               Expanded(
-                  child: Container(
-                // color: Colors.blue,
-                // child: Center(
-                //   child: Text(_kStrings),
-                // ),
-                  child: ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (context, index){
-                    return Container();
-                  }),
-              )),
+                  child: GestureDetector(
+                    onTap: ()=> FocusScope.of(context).unfocus(),
+                    child: ListView.builder(
+                        controller: scrollController,
+                        itemCount: _historyList.length,
+                        itemBuilder: (context, index){
+                          if(_historyList[index].role=="user"){
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              child: Row(
+                                children: [
+                                  const CircleAvatar(),
+                                  const SizedBox(width: 8,),
+                                  Expanded(child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      const Text("User"),
+                                      Text(_historyList[index].content),
+                                    ],
+                                  ))
+                                ],
+                              ),
+                            );
+                          }
+                      return Row(
+                        children: [
+                          const CircleAvatar(
+                            backgroundColor: Colors.green,
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Expanded(child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("ChatGPT"),
+                              Text(_historyList[index].content)
+                            ],
+                          ))
+                        ],
+                      );
+                    }),
+                  )),
               Row(
                 children: [
                   Expanded(
                     child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(32),
                         border: Border.all()
                       ),
                       child: TextField(
                         controller: messageTextController,
-                        decoration: InputDecoration(
+                        decoration: const InputDecoration(
                             border: InputBorder.none, hintText: "Message"),
                       ),
                     ),
                   ),
                   IconButton(
                     iconSize: 42,
-                      onPressed: () {},
-                      icon: Icon(Icons.arrow_circle_up))
+                      onPressed: () async{
+                      if(messageTextController.text.isEmpty){
+                        return;
+                      }
+                      setState(() {
+                        _historyList.add(Messages(role: "user", content: messageTextController.text.trim()));
+                        _historyList.add(Messages(role: "assistant", content: "content"));
+                      });
+                      try{
+                        var text="";
+                        final stream = requestChatStream(messageTextController.text.trim());
+                        await for(final textChunk in stream){
+                          text += textChunk;
+                          setState(() {
+                            _historyList.last = _historyList.last.copyWith(
+                              content: text
+                            );
+                            _scrollDown();
+                          });
+                        }
+                        // await requestChat(messageTextController.text.trim());
+                        messageTextController.clear();
+                        streamText="";
+                      }catch(e){
+                        print(e.toString());
+                      }
+                      },
+                      icon: const Icon(Icons.arrow_circle_up))
                 ],
               )
             ],
