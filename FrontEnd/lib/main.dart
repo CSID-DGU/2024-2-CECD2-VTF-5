bbae75dded0914f5eab1cd3b77f1d0843b902ef6
Jeong_Hwanuk <udown0109@naver.com>
2024-09-20 16:35:40 +0900

    .

--- FrontEnd/lib/main.dart
+++ FrontEnd/lib/main.dart
@@ -32,28 +32,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController messageTextController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+
+  static const String _kStrings = "chat bot";
+  String get _currentString => _kStrings;
+
+  @override
+  void dispose() {
+    messageTextController.dispose();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController messageTextController = TextEditingController();
+
+
     return Scaffold(
-      body: Center(
-        child: Column(
-
-
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerRight,
+                child: Card(
+                  child: PopupMenuButton(itemBuilder: (context) {
+                    return [
+                      PopupMenuItem(
+                          child: ListTile(
+                        title: const Text("히스토리"),
+                      )),
+                      PopupMenuItem(
+                          child: ListTile(
+                            title: const Text("새 채팅"),
+                          ))
+                    ];
+                  }),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
