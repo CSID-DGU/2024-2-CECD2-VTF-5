db76d862b5396eafb87273f3708c4466efebc083
seoheeee <4644dbfan@naver.com>
2024-10-09 15:44:30 +0900

    Rename FrontEnd to frontend

---
+++ frontend/android/app/src/main/kotlin/com/vtfecho/frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.vtfecho.frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- frontend/lib/screens/loading.dart
+++ frontend/lib/screens/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Loading extends StatelessWidget {
   const Loading({super.key});
 
