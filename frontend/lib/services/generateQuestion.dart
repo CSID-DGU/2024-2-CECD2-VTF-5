077dd596b40b2e0945b410a3491f1d52aa3c3c0c
Jeong_Hwanuk <udown0109@naver.com>
2024-11-16 16:18:25 +0900

    genQ file

--- frontend/lib/services/generateQuestion.dart
+++ frontend/lib/services/generateQuestion.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../service/recording_service.dart';
+import './recording_service.dart';
 
 class genQuestionService {
   List<String> useResponses() {
