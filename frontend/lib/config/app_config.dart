aedad4a1792f39754d47aac27836957c2bae043d
Jeong_Hwanuk <udown0109@naver.com>
2024-11-22 18:00:36 +0900

    큰일날뻔

--- frontend/lib/config/app_config.dart
+++ frontend/lib/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   // 서버 기본 URL
-  static const String apiBaseUrl = "https://two024-2-cecd2-vtf-5.onrender.com";
+  static const String apiBaseUrl = "";
   //~~.com으로 끝나도록 .com/로 끝나면 안됨!!
 }
